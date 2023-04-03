0c02cc0450a0f43bc270aae28625a8ac6ed23c7c
Angela Canning <46754565+AngelaMaC@users.noreply.github.com>
2023-04-03 13:58:59 -0500

    Talk further script

---
+++ novellus-scripts.js
@@ -0,0 +1,12 @@
+// Talk Further Novellus - all communities
+(function () {
+    var a = document.createElement("script");
+    var b = document.getElementsByTagName("script")[0];
+    a.type = "text/javascript";
+    a.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + "js.talkfurther.com/talkfurther_init.min.js";
+    a.async = true;
+    b.parentNode.insertBefore(a, b);
+  }
+  )();
+
+  
